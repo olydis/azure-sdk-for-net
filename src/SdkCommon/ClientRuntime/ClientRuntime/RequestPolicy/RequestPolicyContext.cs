96aa9172ece28d9628f7f6f7ec839dbe5092f278
Johannes Bader <jobader@microsoft.com>
2017-09-08 10:57:17 -0700

    wip

---
+++ src/SdkCommon/ClientRuntime/ClientRuntime/RequestPolicy/RequestPolicyContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Rest.ClientRuntime.RequestPolicy
+{
+    public class Context
+    {
+        public static readonly Context None = new Context(CancellationToken.None);
+
+        public CancellationToken CancellationToken { get; private set; }
+
+        public Context(CancellationToken cancellationToken)
+        {
+            CancellationToken = cancellationToken;
+        }
+
+        public Context WithNewCancellationToken(CancellationToken newCancellationToken) => new Context(newCancellationToken);
+
+        public static implicit operator Context(CancellationToken ct) => new Context(ct);
+    }
+}
---
+++ src/SdkCommon/ClientRuntime/ClientRuntime/RequestPolicy/TransientFailurePolicy.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Rest.TransientFaultHandling;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Microsoft.Rest.ClientRuntime.RequestPolicy
+{
+    public interface IRetryStrategy
+    {
+        RetryCondition 
+    }
+
+    public static class RetryPolicy
+    {
+        public static IRequestPolicy Create(IRequestPolicy next, IRetryStrategy strategy)
+            => new Policy(
+                next ?? throw new ArgumentNullException(nameof(next)),
+                strategy ?? throw new ArgumentNullException(nameof(strategy))
+            );
+
+        private class Policy : IRequestPolicy
+        {
+            private readonly IRequestPolicy next;
+            private readonly IRetryStrategy strategy;
+
+            public Policy(IRequestPolicy next, IRetryStrategy strategy)
+            {
+                this.next = next;
+                this.strategy = strategy;
+            }
+
+            public async Task<HttpResponseMessage> SendAsync(Context context, HttpRequestMessage request)
+            {
+                // TODO: make serious
+                Console.WriteLine("Sending request");
+                var result = await next.SendAsync(context, request);
+                Console.WriteLine("Received response");
+                return result;
+            }
+        }
+    }
+}
